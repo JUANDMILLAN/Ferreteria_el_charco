3055d1c54337a438f12579896d1364a18718b81b
Juan Millan <jdiegomillan123@gmail.com>
2025-03-27 17:55:25 -0500

    se realiza personalcharco

---
+++ src/Personalcharco/Personalcharco.java
@@ -0,0 +1,41 @@
+package Personalcharco;
+
+public class Personalcharco {
+    int id;
+    String nombre;
+    String cargo;
+    double salario;
+
+    public Personalcharco(int id, String nombre, String cargo, double salario){
+        this.id = id;
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+     public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getcargo() {
+        return cargo;
+    }
+    public void setcargo(String  cargo) {
+        this.cargo = cargo;
+    }
+    public double getsalario() {
+        return salario;
+    }
+    public void setsalario(double salario) {
+        this.salario = salario;
+    }
+
+
+}
---
+++ src/Personalcharco/PersonalcharcoDAO.java
@@ -0,0 +1,134 @@
+package Personalcharco;
+
+
+
+import Conexion.ConexionBD;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+
+
+
+
+public class PersonalcharcoDAO
+{
+    private ConexionBD conexionBD = new ConexionBD();
+
+    // Metodo para agregar un Personal
+    public void agregar(Personalcharco personalcharco)
+    {
+        Connection con = conexionBD.getConnection();
+        String query = "INSERT INTO personal (nombre, cargo, salario) VALUES (?, ?, ?)";
+
+        try
+        {
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setString(1,personalcharco.getNombre());
+            pst.setString(2, personalcharco.getcargo());
+            pst.setDouble(3, personalcharco.getsalario());
+
+            int resultado = pst.executeUpdate();
+            if (resultado > 0)
+            {
+                JOptionPane.showMessageDialog(null, "empleado ha sido agregado correctamente");
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Error al agregar empleado.");
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        try
+        {
+            if (con != null) con.close();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    // Metodo para eliminar un cliente
+    public void eliminar(int id)
+    {
+        Connection con = conexionBD.getConnection();
+        String query = "DELETE FROM personal WHERE id = ?";
+
+        try
+        {
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setInt(1, id);
+
+            int resultado = pst.executeUpdate();
+            if (resultado > 0)
+            {
+                JOptionPane.showMessageDialog(null, "personal eliminado correctamente.");
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Error al eliminar el personal.");
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        try
+        {
+            if (con != null) con.close();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    // Metodo para actualizar un cliente
+    public void actualizar(Personalcharco personalcharco) {
+        Connection con = conexionBD.getConnection();
+        String query = "UPDATE personal SET nombre = ?, cargo = ?, salario = ? WHERE id = ?";
+
+
+        try
+        {
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setString(1, personalcharco.getNombre());
+            pst.setString(2, personalcharco.getcargo());
+            pst.setDouble(3, personalcharco.getsalario());
+            pst.setInt(4, personalcharco.getId());
+
+            int resultado = pst.executeUpdate();
+            if (resultado > 0)
+            {
+                JOptionPane.showMessageDialog(null, "personal actualizado correctamente.");
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Error al actualizar personal.");
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        try
+        {
+            if (con != null) con.close();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
+
